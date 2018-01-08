301b738c4c0d539203c700f16cf11af59807a60e
звездочёт <mykaralw@yandex.ru>
2018-01-08 16:47:53 +0300

    0.20180108
    Current Locale

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 Console CharMap  
-0.20180105
+0.20180108
 
 # Console CharMap
 
 Bash script for view charmap in console.
 
+Example:
+
+```bash
+$ bash charmap.sh 0040
+
+UTF-8		Console CharMap		0.20180108
+
+0040: @ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~.
+0080: ...
+```
+
 ---  
 2018  
 zvezdochiot
+
--- charmap.sh
+++ charmap.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-printf "\n\t\tConsole CharMap\t\t0.20180105\n"
+tlocale=$(echo "${LANG//*.}")
+
+printf "\n$tlocale\t\tConsole CharMap\t\t0.20180108\n"
 
 hb=$1
 [[ -z $hb ]] && hb=0
 printf -v xb "%d" "0x$hb"
 
-for ii in {0..2047}
-do
-    if [[ $ii -eq 0 || $(((ii+xb)%64)) -eq 0 ]]
-    then
-        printf "\n%04x: " $((ii+xb))
-    fi
-    printf -v hb "%x" $((ii+xb))
-    printf "\u$hb"
-done
+
+if [[ "$tlocale" = "UTF-8" ]]
+then
+    for ii in {0..2047}
+    do
+        if [[ $ii -eq 0 || $(((ii+xb)%64)) -eq 0 ]]
+        then
+            printf "\n%04x: " $((ii+xb))
+        fi
+        printf -v hb "%x" $((ii+xb))
+        printf "\u$hb"
+    done
+else
+    for ii in {0..255}
+    do
+        if [[ $ii -eq 0 || $(((ii)%64)) -eq 0 ]]
+        then
+            printf "\n%04x: " $((ii))
+        fi
+        printf -v hb "%x" $((ii))
+        printf "\u$hb"
+    done
+fi
 printf "\n"
